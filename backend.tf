ece0c8049bfc35dffbc186fd61fd4c5bcff185be
PrasanthKumar0 <142900866+PrasanthKumar0@users.noreply.github.com>
2023-11-20 11:25:39 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "prasanthkumar.000"
+    key            = "terraform1.tfstate"
+    region         = "ap-south-1" 
+    encrypt        = false
+  }
+}
