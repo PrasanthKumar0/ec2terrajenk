1e857923728ed7a4486bf8f0c475631d1aa5ff4a
PrasanthKumar0 <142900866+PrasanthKumar0@users.noreply.github.com>
2023-11-20 11:41:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,5 @@
-terraform {
-  backend "s3" {
-    bucket         = "prasanthkumar.000"
-    key            = "terraform1.tfstate"
-    region         = "ap-south-1" 
-  }
+backend "s3" {
+  bucket         = "prasanthkumar.000"
+  key            = "terraform1.tfstate"
+  region         = "ap-south-1" 
 }
