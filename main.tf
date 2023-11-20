07e14d14b194a66c63c0e7ac90f2c4330d5d0636
PrasanthKumar0 <142900866+PrasanthKumar0@users.noreply.github.com>
2023-11-20 11:47:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# main.tf
+
+terraform {
+  backend "s3" {
+    bucket         = "prasanthkumar.000"
+    key            = "terraform1.tfstate"
+    region         = "ap-south-1" 
+  }
+}
 provider "aws" {
   region = "ap-south-1"
 }
